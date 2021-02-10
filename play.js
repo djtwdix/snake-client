bad6b23e2c3308b82b38cc3ab42d3e3a8b3558b9
djtwdix <djtwd.ix@icloud.com>
2021-02-10 13:48:16 -0400

    Seperated connect function into seperate client.js file

--- play.js
+++ play.js
@@ -1,17 +1,4 @@
-const net = require('net');
-const stdin = process.stdin;
+const connect = require("./client");
 
-const connect = () => {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-  return conn;
-};
-
-console.log('Connecting...');
+console.log("Connecting...")
 connect();
