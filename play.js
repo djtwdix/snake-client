d9eb9b335eb681ed149aadb8b39bee40d281127b
djtwdix <djtwd.ix@icloud.com>
2021-02-10 14:32:29 -0400

    Set up user input and handling of user input functions

--- play.js
+++ play.js
@@ -1,4 +1,19 @@
 const connect = require("./client");
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  process.stdin.on('data', handleUserInput);
+  return stdin
+}
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
 
 console.log("Connecting...")
 connect();
+setupInput();
